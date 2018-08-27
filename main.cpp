9f99692260be1aac90ce1b227268f8b8d8df6bfb
Marco Meza <mezamarco@yahoo.com>
2018-08-27 11:39:57 -0700

    Second commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 
-
-
 #include <iostream>
 #include <vector>
 #include <unordered_map>
 #include <string>
+#include <ctime>
 
 //We need an Vehicle abstract class
 
@@ -21,6 +20,45 @@ public:
   //This pure virtual function will make this class an abstract class
   //Therefore no constructor or destructor is needed
   virtual void toString() = 0;
+  virtual void printPlate() = 0;
+  
+  
+  int getSize(){
+  	return size;
+  }
+  std::vector<int> getLocation(){
+  	std::vector<int> myLocation  = {location[0], location[1]};
+	return myLocation;
+  }
+  void setLocation(int i, int j){
+  	this->location[0] = i;
+	this->location[1] = j;
+	return;
+  }
+
+  std::string getRandomPlate(){
+
+	std::string plate = "";
+  	for(int i = 0; i < 7; i++ )
+  	{
+		int val;
+		//The only valid characters: [48,57] or [65,90]
+		do{
+		  val = (rand() % 43) + 48;
+		}while( val>=58 && val <= 64);
+
+		//We have a valid number, convert into a character and then add to plate
+		char ch = (char) val;
+		plate = plate + ch;	
+		
+	}
+	return plate;
+  
+  }
+
+
+
+
 };
 
 
@@ -34,8 +72,8 @@ public:
 class Truck : public Vehicle{
 public:
 	//Custom Constructor
-	Truck(std::string plate){
-	  this->licensePlate = plate;
+	Truck(){
+	  this->licensePlate = getRandomPlate();
 	  this->isParked = 0;
 	  this->size = 5;
 
@@ -56,6 +94,10 @@ public:
 			<< " , Location : " << location[0] << ","<< location[1]
 			<< "\n\n";
 	}
+  
+	void printPlate(){
+	  std::cout << "Truck :\t\t" << licensePlate << "\n";
+	}
 
 };
 
@@ -64,8 +106,8 @@ class Car : public Vehicle{
 
 public:
 	//Custom Constructor
-	Car(std::string plate){
-	  this->licensePlate = plate;
+	Car(){
+	  this->licensePlate = getRandomPlate();
 	  this->isParked = 0;
 	  this->size = 3;
 	  this->location[0] = -1;
@@ -85,6 +127,10 @@ public:
 			<< " , Location : " << location[0] << ","<< location[1]
 			<< "\n\n";
 	}
+	
+	void printPlate(){
+	  std::cout << "Car :\t\t" << licensePlate << "\n";
+	}
 
 };
 
@@ -93,8 +139,8 @@ class Motorcycle : public Vehicle{
 
 public:
 	//Custom Constructor
-	Motorcycle(std::string plate){
-	  this->licensePlate = plate;
+	Motorcycle(){
+	  this->licensePlate = getRandomPlate();
 	  this->isParked = 0;
 	  this->size = 1;
 	  this->location[0] = -1;
@@ -113,6 +159,10 @@ public:
 		      	<< "Is it parked : " << isParked
 			<< " , Location : " << location[0] << "," << location[1]
 			<< "\n\n";
+	}
+	
+	void printPlate(){
+	  std::cout << "Motorcycle :\t" << licensePlate << "\n";
 	}
 
 };
@@ -133,54 +183,277 @@ private:
 	bool isFull;
 
 	int availableSpots;
-
+	int vehicleCounter;
 public:
 	ParkingLot(){
 	  
-	  clearParkingLot(this->arr);
+	  clearParkingLot();
 
 	  this->isEmpty = 1;
 	  this->isFull = 0;
-	  this->availableSpots = 100; 
+	  this->availableSpots = 100;
+	  this->vehicleCounter = 0; 
 	}
 	~ParkingLot(){
 	  //We need to delete all the Vehicles that appear in our parking lot
 	}
 
 
+	void clearParkingLot();	
 	void printParkingLot();
 	int getNumOfAvailableSpots();
-
-	void clearParkingLot(Vehicle* arr[5][20]);	
-
+	void tellMeTheAvailableSpots();
+	bool canFit(int i, int j,int size);
+	
+	bool parkTheVehicle(Vehicle* vehicle);
 };
 
 
 
+//Main function prototypes
+void populateVehicleVector(std::vector<Vehicle*>& myVehicleVect, int vehicleLimit);
+void displayVehicleVector(std::vector<Vehicle*>& myVect);
+
 
 int main(){
+
+  //Required to generate random elements
+  srand(time(NULL));
+
 
 
 
   ParkingLot theLot;
 
-  std::cout << "HELLO \n\n\n";
+  //Lets build a vector of pointers to random vehicles
+  int vehicleLimit;
+  std::cout << "\n\nHow many random vehicles should we create: ";
+  std::cin >> vehicleLimit;
+
+  std::vector<Vehicle*> myVehicleVect;
+
+  
+  populateVehicleVector(myVehicleVect, vehicleLimit);
+
+  //we currently have a vector of vehicle pointers
+
+  //What are the vehicles that we created?
+  displayVehicleVector(myVehicleVect);
 
 
+
+
+
+
+
+  theLot.printParkingLot();
+  theLot.tellMeTheAvailableSpots();
+  std::cout << "\n\n";
   return 0;
 }
 
+  
+
+void populateVehicleVector(std::vector<Vehicle*>& myVehicleVect, int vehicleLimit){
+
+	  for(int i = 0; i < vehicleLimit; i++){
+  	 	 
+		  //Range : {1,2,3}
+	 	 int randElement = (rand() % 3) + 1;	
+  
+ 	 	  switch(randElement){
+	  
+		  	case 1:
+				//Build a Motorcycle
+				myVehicleVect.push_back(new Motorcycle());
+				break;
+			
+			case 2:
+				//Build a Car
+				myVehicleVect.push_back(new Car());
+				break;
+
+			case 3:
+				//Build a Truck
+				myVehicleVect.push_back(new Truck());
+				break;
+
+	  
+	  		default:
+				break;
+	  	
+	  	}
+  
+  	}	
+	return;
+}
 
 
-void ParkingLot::clearParkingLot(Vehicle* arr[5][20]){
+void displayVehicleVector(std::vector<Vehicle*>& myVect){
+	
+	std::cout << "We are printing all our vehicles:\n";	
+	for(Vehicle* temp : myVect){
+		temp->printPlate();
+	}	
+	return;
+}
 
-	std::cout << "\nClearing the parking lot\n";
 
+
+
+
+void ParkingLot::clearParkingLot(){
 	for(int i = 0; i < 5; i++){
 		for(int j = 0; j < 20; j++ ){
-			arr[i][j] = nullptr;
+			this->arr[i][j] = nullptr;
+		}
+	}
+	this->isEmpty = 1;
+	this->vehicleCounter = 0;
+	
+}
+
+
+
+void ParkingLot::printParkingLot(){
+
+	std::cout << "\n\n Printing Parking Lot: \n";
+	//Display the parking lot
+	for(int  i = 0; i <5; i++ ){
+	  std::cout << " ";
+	  for(int j = 0; j < 20 ; j++ ){
+	
+	  		  
+		if(arr[i][j]){
+		  std::cout << "1" << "  ";
+		} 
+		else{
+		  std::cout << "0" << "  ";
+		}
+	  }
+	  std::cout << "\n"; 
+	}
+
+
+}
+
+int ParkingLot::getNumOfAvailableSpots(){
+	return this->availableSpots; 
+}
+
+	
+void ParkingLot::tellMeTheAvailableSpots(){
+
+	std::cout << "\n\nAvailable Spots: " << availableSpots << "\n\n";
+	
+	std::vector< std::vector<int> > myVect(5);
+	
+	
+	
+	//Display the parking lot
+	for(int  i = 0; i <5; i++ ){
+	  for(int j = 0; j < 20 ; j++ ){
+		if(arr[i][j] == nullptr){
+		  myVect[i].push_back(j);	
+		} 
+	  }
+	}
+	
+
+	for(int i = 0; i < 5; i++){
+		int counter = 0;
+		std::cout << "\nFLOOR "<< i << " :\n\t";
+		
+		for(int k : myVect[i]){
+			
+			
+			std::cout << "["<< i << "," << k << "]\t";
+			++counter;
+
+			if(counter % 10 == 0){
+			  std::cout << "\n";
+		          std::cout << "\t";
+			}
+		}
+	}
+}
+
+
+bool ParkingLot::parkTheVehicle(Vehicle* vehicle){
+
+	//We need to take into account the size of the vehicle
+	//and the avaiable spots in the parking lot
+	
+	int size = vehicle->getSize();
+
+	if(size > availableSpots)
+	{ 
+		return 0;
+	}
+
+	//Only for the first case that the parking lot is empty
+	if(isEmpty == 1){
+		
+		for(int i = 0; i < size; i++){
+		  arr[0][i] = vehicle;
+		  --availableSpots;
+		}
+		vehicle->setLocation(0,0);
+
+		++vehicleCounter;
+
+		isEmpty = 0;
+		return 1;
+	}
+
+
+	//Now go park the car, find the first available location, and check if it fits.
+	for(int i = 0; i<5; i++ ){
+		for(int j = 0; j <= 20 - size; i++){
+			
+			if(arr[i][j] == nullptr){
+			  	
+				if( canFit(i,j,size) ){
+					//Our vehicle fits on the following spots
+					int limit = j+size;
+
+					for(int m = j; m < limit ;m++){
+						arr[i][m] = vehicle;
+						--availableSpots;
+					}
+					vehicle->setLocation(i,j);
+					++vehicleCounter;
+
+					return 1;
+				}
+			}
+		        
+		
+		}
+	
+	}
+	//Our vehicle does not fit in the parking lot
+	return 0;
+}
+
+
+bool ParkingLot::canFit(int i, int j,int size){
+	
+	int lastIndex = j+size -1;
+	if(lastIndex >19){
+		return 0;
+	}
+	
+	
+	//Check the 2D array, we need the following spots to be free
+	for( int y = j ; y <= lastIndex; ++y){
+		if(arr[i][y] != nullptr){
+		  return 0;
 		}
 	}
 
-	
+	return 1;
+
 }
+
+
